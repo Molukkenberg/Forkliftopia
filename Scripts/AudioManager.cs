e6194ee1271c9fc8c526a23fab3d74a21bea6488
Mika Kruckenberg <71791126+Molukkenberg@users.noreply.github.com>
2023-05-02 19:51:59 +0200

    Update AudioManager.cs

--- Scripts/AudioManager.cs
+++ Scripts/AudioManager.cs
@@ -6,17 +6,6 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private AudioSource _audioSource;
     
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Modify(int Value)
     {
         _audioSource.volume= Value;
